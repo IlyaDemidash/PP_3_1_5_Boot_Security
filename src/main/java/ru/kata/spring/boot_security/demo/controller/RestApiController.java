46a401d6129e4d4a618b22def9386086e7d5e007
ilyaryhth <cool.clarion@yandex.ru>
2022-11-06 11:47:23 +0400

    correction Controller

--- src/main/java/ru/kata/spring/boot_security/demo/controller/RestApiController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RestApiController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import ru.kata.spring.boot_security.demo.entities.Role;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -12,54 +13,71 @@ import java.security.Principal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
-public class MainController {
+public class RestApiController {
     private final UserService userService;
 
     @Autowired
-    public MainController(UserService userService) {
+    public RestApiController(UserService userService) {
         this.userService = userService;
     }
 
-    //Возвращаем список пользователей для заполнения форм страницы allusers
+    @GetMapping("/")
+    public String MainPage() {
+        return "redirect:/login";
+    }
+
     @GetMapping("/admin")
+    public ModelAndView adminPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("all_users");
+        return modelAndView;
+    }
+
+    @GetMapping("/user")
+    public ModelAndView userPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user_only_info");
+        return modelAndView;
+    }
+
+    //Возвращаем список пользователей для заполнения форм страницы allusers
+    @GetMapping("/api/admin")
     public ResponseEntity<List<User>> getUserForAdminPage() {
         List<User> users = userService.findAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     //Возвращаем юзера для заполнения форм страницы user_only_info
-    @GetMapping("/user")
+    @GetMapping("/api/user")
     public ResponseEntity<User> getUser(Principal principal) {
         return new ResponseEntity<>(userService.findByEmail(principal.getName()), HttpStatus.OK);
     }
 
     //Возвращаем спилок существующих ролей
-    @GetMapping("/roles")
+    @GetMapping("/api/roles")
     public ResponseEntity<List<Role>> getAllRoles() {
         return new ResponseEntity<>(userService.roleList(), HttpStatus.OK);
     }
 
     //Получаем пользователя по id
-    @GetMapping("/admin/{id}")
+    @GetMapping("/api/admin/{id}")
     public User getUserById(@PathVariable Long id) {
-        User user = userService.findById(id);
-        return user;
+        return userService.findById(id);
     }
 
-    @PostMapping(value = "/admin")
+    @PostMapping(value = "/api/admin")
     public ResponseEntity<User> addUserAction(@RequestBody User user) {
         userService.saveUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PutMapping("/admin")
+    @PutMapping("/api/admin")
     public ResponseEntity<User> updateUser(@RequestBody User user) {
         userService.updateUser(user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping("/admin/{id}")
+    @DeleteMapping("/api/admin/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
